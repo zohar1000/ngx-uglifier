770192c7d37ab54f1c26c859365e2ed103798c3d
Zohar Sabari <zohar1000@gmail.com>
2021-04-19 18:09:22 +0300

    changed all fs to await

--- projects/ngx-uglifier/src/lib/ngx-uglifier.ts
+++ projects/ngx-uglifier/src/lib/ngx-uglifier.ts
@@ -1,6 +1,6 @@
 declare var require: any;
 const { minify } = require('terser');
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 import { NgxUglifierConfig, NgxUglifierOptions } from './ngx-uglifier-config.model';
 
@@ -20,8 +20,6 @@ export class NgxUglifier {
     this.destFolder = `${this.currDestParentFolder}/${config.projectName}`;
     this.isLegacyAccessorsDefinition = !(config?.uglifyOptions?.isLegacyAccessorsDefinition === false);
     this.options = this.getOptions(config?.uglifyOptions);
-    this.verifyDirectory(this.currDestParentFolder);
-    this.verifyDirectory(this.destFolder);
   }
 
   setConfigDefaults(config: NgxUglifierConfig) {
@@ -31,69 +29,67 @@ export class NgxUglifier {
   }
 
   async init() {
-    this.copyFolder(this.srcFolder, this.currDestParentFolder, { clearFolder: this.destFolder });
-    this.deleteSourceMaps(this.destFolder);
-    await this.obfuscate(this.destFolder);
+    await this.copyFolder(this.srcFolder, this.currDestParentFolder, { clearFolder: this.destFolder });
+    await this.deleteSourceMaps(this.destFolder);
+    await this.uglify(this.destFolder);
     if (this.config.destParentFolder === this.config.srcParentFolder) {
-      this.copyFolder(this.destFolder, this.config.srcParentFolder, { clearFolder: this.srcFolder });
-      fs.rmdirSync(this.currDestParentFolder, { recursive: true });
+      await this.copyFolder(this.destFolder, this.config.srcParentFolder, { clearFolder: this.srcFolder });
+      await fsPromises.rmdir(this.currDestParentFolder, { recursive: true });
     }
   }
 
-  clearFolder(folder) {
-    const entries = fs.readdirSync(folder, {withFileTypes: true});
+  async clearFolder(folder) {
+    const entries = await fsPromises.readdir(folder, {withFileTypes: true});
     for (const entry of entries) {
       const folderPath = `${folder}/${entry.name}`;
-      if (this.isResourceDirectory(folderPath)) {
-        fs.rmdirSync(folderPath, { recursive: true });
+      if (await this.isResourceDirectory(folderPath)) {
+        await fsPromises.rmdir(folderPath, { recursive: true });
       } else {
-        fs.unlinkSync(folderPath);
+        await fsPromises.unlink(folderPath);
       }
     }
   }
 
-  deleteSourceMaps(folder) {
-    const entries = fs.readdirSync(folder, {withFileTypes: true});
+  async deleteSourceMaps(folder) {
+    const entries = await fsPromises.readdir(folder, {withFileTypes: true});
     for (const entry of entries) {
       if (entry.isFile()) {
         if (entry.name.endsWith('.map')) {
-          fs.unlinkSync(`${folder}/${entry.name}`);
+          await fsPromises.unlink(`${folder}/${entry.name}`);
         }
       } else {
-        this.deleteSourceMaps(`${folder}/${entry.name}`);
+        await this.deleteSourceMaps(`${folder}/${entry.name}`);
       }
     }
   }
 
-  async obfuscate(folder) {
-    const entries = fs.readdirSync(folder, {withFileTypes: true});
+  async uglify(folder) {
+    const entries = await fsPromises.readdir(folder, {withFileTypes: true});
     for (const entry of entries) {
       if (entry.isFile()) {
         if (entry.name.endsWith('.js')) {
           const filePath = `${folder}/${entry.name}`;
-          const fileText = fs.readFileSync(filePath, 'utf8');
+          const fileText = await fsPromises.readFile(filePath, 'utf8');
           const result = await minify(fileText, this.options);
-          fs.writeFileSync(filePath, result.code, 'utf8');
+          await fsPromises.writeFile(filePath, result.code, 'utf8');
         } else if (entry.name.endsWith('.d.ts')) {
           const TYPES = ['component', 'directive', 'pipe', 'service', 'guard', 'resolver'];
           if (TYPES.find(type => entry.name.endsWith(`.${type}.d.ts`))) {
-            this.obfuscateDefinitionFile(path.join(folder, entry.name));
+            await this.adjustDefinitionFile(path.join(folder, entry.name));
           }
         }
       } else {
-        await this.obfuscate(`${folder}/${entry.name}`);
+        await this.uglify(`${folder}/${entry.name}`);
       }
     }
   }
 
-  obfuscateDefinitionFile(filePath) {
+  async adjustDefinitionFile(filePath) {
     const PRIVATE_SUFFIXES = ['private ', 'private async ', 'protected ', 'protected async ', '_'];
-    let fileText = fs.readFileSync(filePath, 'utf8');
+    let fileText = await fsPromises.readFile(filePath, 'utf8');
     const crlf = fileText.indexOf('\r\n') !== -1 ? '\r\n' : '\n';
     const lines = fileText.split(crlf);
-    if (lines.length === 0) {
-      return;
-    }
+    if (lines.length === 0) return;
     for (let i = lines.length - 1; i >= 0; i--) {
       const line = lines[i].trimLeft();
       if (PRIVATE_SUFFIXES.find(suffix => line.startsWith(suffix))) {
@@ -115,67 +111,62 @@ export class NgxUglifier {
       }
     }
     fileText = lines.join('\n');
-    fs.writeFileSync(filePath, fileText, 'utf8');
+    await fsPromises.writeFile(filePath, fileText, 'utf8');
   }
 
   /*********************/
   /*      C O P Y      */
   /*********************/
 
-  copyFolder(source, target, opts?) {
-    if (opts?.clearFolder) this.clearFolder(opts.clearFolder);
+  async copyFolder(source, target, opts?) {
+    if (opts?.clearFolder) await this.clearFolder(opts.clearFolder);
     let files = [];
     const targetFolder = path.join(target, path.basename(source));
-    if (this.verifyDirectory(targetFolder)) {
-      files = fs.readdirSync(source);
+    if (await this.verifyDirectory(targetFolder)) {
+      files = await fsPromises.readdir(source);
       for (const file of files) {
         const curSource = path.join(source, file);
         const isDirectory = this.isResourceDirectory(curSource);
         if (isDirectory) {
-          this.copyFolder(curSource, targetFolder);
+          await this.copyFolder(curSource, targetFolder);
         } else {
-          this.copyFileSync(curSource, path.join(targetFolder, file));
+          await this.copyFile(curSource, path.join(targetFolder, file));
         }
       }
     }
   }
 
-  copyFileSync(from, to) {
-    const fileText = fs.readFileSync(from, 'utf8');
-    fs.writeFileSync(to, fileText);
+  async copyFile(from, to) {
+    const fileText = await fsPromises.readFile(from, 'utf8');
+    await fsPromises.writeFile(to, fileText);
   }
 
-  getResourceStat(resourcePath) {
+  async getResourceStat(resourcePath) {
     let stat;
     try {
-      stat = fs.statSync(resourcePath);
+      stat = await fsPromises.stat(resourcePath);
     } catch (e) {
     }
     return stat;
   }
 
-  isResourceExist(resourcePath) {
-    const stat = this.getResourceStat(resourcePath);
-    return Boolean(stat);
-  }
-
-  isResourceDirectory(resourcePath) {
-    const stat = this.getResourceStat(resourcePath);
-    return !stat ? false : stat.isDirectory();
-  }
-
-  verifyDirectory(resourcePath, isMakeDir = true) {
+  async verifyDirectory(resourcePath, isMakeDir = true) {
     let isDirectory;
-    const stat = this.getResourceStat(resourcePath);
+    const stat = await this.getResourceStat(resourcePath);
     if (!stat) {
       if (isMakeDir) {
-        fs.mkdirSync(resourcePath);
+        await fsPromises.mkdir(resourcePath);
         isDirectory = true;
       }
     } else {
       isDirectory = stat.isDirectory();
     }
     return isDirectory;
+  }
+
+  async isResourceDirectory(resourcePath) {
+    const stat = await this.getResourceStat(resourcePath);
+    return !stat ? false : stat.isDirectory();
   }
 
   getOptions(options: NgxUglifierOptions = {}) {
